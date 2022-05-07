4ffe3b79493a9d1d745896c94433047ceb30455a
jigaowan <fmh1992@outlook.com>
2022-05-07 17:05:10 +0800

    update sync calibre util

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# calibre_util
-sync new dir/file use realname to easy find book by not support OPDS soft
+# manga-utils
+
+## sync_library
+
+sync calibre dir to new dir/file use realname to easy find book by not support OPDS soft
+
+```
+python3 sync_library.py your_calibre_dir your_new_manga_dir
+```
--- sync_library.py
+++ sync_library.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import sqlite3
 from shutil import copyfile
 
@@ -21,11 +22,30 @@ db_file = 'metadata.db'
 old_path = '/volume1/share/books'
 new_path = '/volume1/share/comic'
 
+if len(sys.argv) not in (1, 3):
+    raise ValueError('wrong args number')
+
+if len(sys.argv) == 3:
+    old_path = sys.argv[1]
+    new_path = sys.argv[2]
+
+if not os.path.isdir(old_path):
+    raise ValueError('source is not dir')
+
+if not os.path.exists(os.path.join(old_path, db_file)):
+    raise ValueError('source is not calibre dir')
+
+print(f'source dir is {old_path}')
+print(f'output dir is {new_path}')
+
 if not os.path.exists(new_path):
+    print(f'not found output dir')
     os.mkdir(new_path)
+    print(f'create output dir success')
 
 with sqlite3.connect(os.path.join(old_path, db_file)) as db:
     books = db.execute(select_sql).fetchall()
+
 for book in books:
     series = book[0]
     title = book[1]
@@ -35,14 +55,22 @@ for book in books:
     book_old = os.path.join(old_path, path, file_name + '.cbz')
     author_path = os.path.join(new_path, author)
     if not os.path.exists(author_path):
+        print(f'not found author path {author}')
         os.mkdir(author_path)
+        print(f'create author path {author} success')
     if not series:
         series = title
     series_path = os.path.join(author_path, series)
     if not os.path.exists(series_path):
+        print(f'not found series path {series}')
         os.mkdir(series_path)
+        print(f'create series path {series} success')
     book_new = os.path.join(series_path, title + '.cbz')
     if os.path.exists(book_new) and os.path.isfile(book_new) and (
             os.path.getsize(book_old) == os.path.getsize(book_new)):
+        print(f'manga {title} is exist')
         continue
+    print(f'not found manga {title}')
     copyfile(book_old, book_new)
+    print(f'link manga {title} success')
+print(f'link calibre mangas library to new dir finish')
